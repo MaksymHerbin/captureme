7ad44f0d2d051f738782c7530237f52ed5270283
maksym.herbin <maxim.gerbin@gmail.com>
2018-04-02 10:36:11 +0200

    Removing connection properties from source code

--- database/liquibase.sh
+++ database/liquibase.sh
@@ -1,3 +1,45 @@
 #!/usr/bin/env bash
 
-java -jar src/main/resources/liquibase/lib/liquibase.jar --defaultsFile src/main/resources/liquibase/liquibase.properties $*
+function prop {
+    grep "${1}" ${2}|cut -d'=' -f2
+}
+
+properties_file=src/main/resources/liquibase/environments/$1/connection.properties
+
+if [ -f ${properties_file} ]; then
+   echo "Loading database connection properties from ${properties_file}"
+
+   url=$(prop 'url' ${properties_file})
+   username=$(prop 'username' ${properties_file})
+   password=$(prop 'password' ${properties_file})
+
+   if [ -f ${url} ]; then
+        echo "Could not find property url in ${properties_file}"
+        exit 1
+   fi
+
+   if [ -f ${username} ]; then
+        echo "Could not find property username in ${properties_file}"
+        exit 1
+   fi
+
+   if [ -f ${password} ]; then
+        echo "Could not find property password in ${properties_file}"
+        exit 1
+   fi
+
+   java -jar src/main/resources/liquibase/lib/liquibase.jar \
+        --defaultsFile src/main/resources/liquibase/liquibase.properties \
+        --username ${username} \
+        --password ${password} \
+        --url ${url} \
+        ${2}
+
+else
+   echo "File ${properties_file} does not exist. Please make sure you are using correct environment and file exists in specified path"
+   echo "Awailable environments:"
+   echo "development"
+   echo "local"
+   exit 1
+fi
+
