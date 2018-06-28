b54241927662294fa0e6b64a996af3b51edd4a53
maksym.herbin <maxim.gerbin@gmail.com>
2018-06-28 22:36:27 +0200

    add ec2 plugin

--- gateway/src/main/java/com/herbinm/edx/captureme/gateway/WebConfig.java
+++ gateway/src/main/java/com/herbinm/edx/captureme/gateway/WebConfig.java
@@ -1,6 +1,9 @@
 package com.herbinm.edx.captureme.gateway;
 
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.AWSXRayRecorderBuilder;
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
+import com.amazonaws.xray.plugins.EC2Plugin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -10,6 +13,11 @@ import javax.servlet.Filter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    static {
+        AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin());
+        AWSXRay.setGlobalRecorder(builder.build());
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
