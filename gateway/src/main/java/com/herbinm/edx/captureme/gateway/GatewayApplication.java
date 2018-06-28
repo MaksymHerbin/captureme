9a8ecd67e80444eae6f185abbbba5d942c217036
maksym.herbin <maxim.gerbin@gmail.com>
2018-06-28 22:10:30 +0200

    Restructure

--- gateway/src/main/java/com/herbinm/edx/captureme/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/herbinm/edx/captureme/gateway/GatewayApplication.java
@@ -2,7 +2,6 @@ package com.herbinm.edx.captureme.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
@@ -10,11 +9,6 @@ public class GatewayApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("main");
     }
 
     public static class SessionAttribute {
--- gateway/src/main/java/com/herbinm/edx/captureme/gateway/WebConfig.java
+++ gateway/src/main/java/com/herbinm/edx/captureme/gateway/WebConfig.java
@@ -3,11 +3,18 @@ package com.herbinm.edx.captureme.gateway;
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("main");
+    }
 
     @Bean
     public Filter TracingFilter() {
