dd69d2cb98bf7d4bd9111d011b7f954a96c1d2ea
Maksym Herbin <maxim.gerbin@gmail.com>
2018-10-21 20:06:55 +0000

    Added Env Variable

--- gateway_user_data.sh
+++ gateway_user_data.sh
@@ -22,13 +22,21 @@ environment=$(aws ec2 describe-tags \
   --filters "Name=resource-type, Values=instance" "Name=resource-id, Values=${instance_id}" "Name=key, Values=environment" \
   --region=us-west-2 \
   --output=text | cut -f5)
+  
+congnito_secret=$(aws ec2 describe-tags \
+  --filters "Name=resource-type, Values=instance" "Name=resource-id, Values=${instance_id}" "Name=key, Values=cognito_secret" \
+  --region=us-west-2 \
+  --output=text | cut -f5)
 
 artifact_key="$application-$version.jar"
 
 echo "Step 2: Downloading artifacts for $application application, version $version: $artifact_key"
 aws s3api get-object --bucket artifacts-hm --key ${artifact_key} /tmp/${artifact_key}
 
-echo "Step 3: Running $application on $environment environment"
+echo "Step 3: Setting environment variables"
+export COGNITO_CLIENT_SECRET=${congnito_secret}
+
+echo "Step 4: Running $application on $environment environment"
 java -jar -Dspring.profiles.active=${environment} /tmp/${artifact_key}
 
 echo '--------------- USER DATA SCRIPT FINISHED -------------'
